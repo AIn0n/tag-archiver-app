b70225cd091e5168551c0b227c1fad28f7086a80
snosek <stefan.nosek1@gmail.com>
2026-07-04 14:54:34 +0000

    add backend container with hot reload

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "github.com/go-fuego/fuego"
+import (
+	"github.com/go-fuego/fuego"
+	"fmt"
+)
+
+const (
+	LOCAL_PORT = 9999
+)
 
 func main() {
-	s := fuego.NewServer()
+	s := fuego.NewServer(
+		fuego.WithAddr(fmt.Sprintf(":%d", LOCAL_PORT)),
+	)
 
 	fuego.Get(s, "/", func(c fuego.ContextNoBody) (string, error) {
 		return "Hello, World!", nil
 	})
 
-	s.Run()
+	err := s.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
