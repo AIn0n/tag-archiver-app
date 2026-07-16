717e2f78c5ffb61205fe7adeb0b2155d99ae50d9
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2026-07-16 10:13:52 +0200

    Added mechanism to handle locale

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tag_archiver_app/assets/i18n/strings.g.dart';
 
 import 'routing/routes.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() =>
+    runApp(ProviderScope(child: TranslationProvider(child: const MyApp())));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized(); // add this
+    LocaleSettings.useDeviceLocale();
     return MaterialApp.router(routerConfig: router);
   }
 }
